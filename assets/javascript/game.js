b8b037bc9735e1cea859abd1769e9e05aa86df19
mmcadet@gmail.com <34467088+mmcadet@users.noreply.github.com>
2018-06-14 23:58:58 -0500

    HW3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,91 +1,51 @@
-/*
-<script type="text/javascript">
+// When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
+
+// When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins)
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessesSoFar = []; 
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; //list of letters to choose from
+
+document.onkeyup = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase(); 
+    var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)]; 
    
-    var letters = ["a", "b", "c"];
-
-    
-    var guessedLetters = [];
-
+    guessesSoFar.push(userGuess); 
    
-    var letterToGuess = null;
-
-    
-    var totalGuesses = 9;
-    var guessesLeft = 9;
-
-    
-    var wins = 0;
-    var losses = 0;
-
-    
-    var updateGuessesLeft = function() {
-      
-     
-      document.querySelector("#guesses-left").innerHTML = guessesLeft;
-    };
-
-    var updateLetterToGuess = function() {
-      
-      this.letterToGuess = this.letters[Math.floor(Math.random() * this.letters.length)];
-    };
-
-    var updateGuessesSoFar = function() {
-     
-      document.querySelector("#guesses-so-far").innerHTML = guessedLetters.join(", ");
-    };
-
-    
-    var reset = function() {
-      totalGuesses = 9;
-      guessesLeft = 9;
-      guessedLetters = [];
-      updateLetterToGuess();
-      updateGuessesLeft();
-      updateGuessesSoFar();
-    };
-
-    
-    updateLetterToGuess();
-    updateGuessesLeft();
-
- 
-    document.onkeyup = function(event) {
-      
-      
-
-     
-      var letter = String.fromCharCode(event.keyCode).toLowerCase();
-
-     
-      guessedLetters.push(letter);
-
-      
-      updateGuessesLeft();
-      updateGuessesSoFar();
-
-
-      
-      if (letter === letterToGuess) {
-
-        
+    if (userGuess == computerGuess) {
         wins++;
-        document.querySelector("#wins").innerHTML = wins;
-
-       
-        reset();
-      }
-
-
-     
-      if (guessesLeft === 0) {
-
-       
+        alert('Good job! You Won!');
+        guessesLeft = 9;
+        guessesSoFar.length = 0; 
+    }
+    else if (guessesLeft == 0){
         losses++;
-        document.querySelector("#losses").innerHTML = losses;
+        alert('Sorry, you lost. Try again!');
+        guessesLeft = 9;
+        guessesSoFar.length = 0;
+    }
+    else if (userGuess !== computerGuess){
+        guessesLeft--;
+    }  
+    
 
-       
-        reset();
-      }
-    };
-  </script>
-  */
+    var html = "<h1>The Psychic Game</h1>" + 
+    "<p>Guess what letter I'm thinking of!</p>" +
+    "<p>Total Wins: " + 
+    wins + 
+    "</p>" +
+    "<p>Total Losses: " + 
+    losses + 
+    "</p>" +
+    "<p>Guesses Left: " + 
+    guessesLeft + 
+    "</p>" +
+    "<p>Your Guesses so far: " +
+    guessesSoFar +
+    "</p>"
+    ;
+    // Placing the html into the game ID
+    document.querySelector('#game').innerHTML = html;
+}
